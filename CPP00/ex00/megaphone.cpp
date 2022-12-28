dec508afe46e1afeaec99ccf9601ffd8b8101fd6
Wolf Mardin <wmardin@c2s15d88.42wolfsburg.de>
2022-12-28 21:57:54 +0100

    auto add & push

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,7 +6,60 @@
 /*   By: wmardin <wmardin@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/28 14:40:57 by wmardin           #+#    #+#             */
-/*   Updated: 2022/12/28 14:40:58 by wmardin          ###   ########.fr       */
+/*   Updated: 2022/12/28 21:57:48 by wmardin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	for (int i = 1; argv[i]; i++)
+	{
+		std::string					str = argv[i];
+		std::string::iterator		it = str.begin();
+		std::string::iterator		it_e = str.end();
+		for (; it != it_e; it++)
+			std::cout << (char)toupper(*it);
+	}
+	std::cout << std::endl;
+}
+
+// int main(int argc, char **argv)
+// {
+// 	if (argc == 1)
+// 	{
+// 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+// 		return (0);
+// 	}
+// 	for (int i = 1; argv[i]; i++)
+// 	{
+// 		for (int j = 0; argv[i][j]; j++)
+// 			std::cout << (char)toupper(argv[i][j]);
+// 		// if (argv[i + 1])
+// 		// 	std::cout << " ";
+// 	}
+// 	std::cout << std::endl;
+
+
+
+// }
+
+void a(char)
+{
+
+}
+void a(int)
+{
+
+}
+void a(int, int)
+{
+
+}
