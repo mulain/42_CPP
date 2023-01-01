e46f89f963f0f321af17080fc45a8dc3af9c6bbe
wolf <wolf.mardin@gmail.com>
2023-01-01 14:20:12 +0100

    auto add & push

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,10 +6,30 @@
 /*   By: wmardin <wmardin@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/28 14:40:57 by wmardin           #+#    #+#             */
-/*   Updated: 2022/12/28 21:57:48 by wmardin          ###   ########.fr       */
+/*   Updated: 2022/12/29 11:40:28 by wmardin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	for (int i = 1; argv[i]; i++)
+ 	{
+ 		for (int j = 0; argv[i][j]; j++)
+			std::cout << (char)toupper(argv[i][j]);
+		/* if (argv[i + 1])
+			std::cout << " "; */
+	}
+	std::cout << std::endl;
+}
+
+/*
 #include <iostream>
 #include <string>
 
@@ -30,36 +50,4 @@ int main(int argc, char **argv)
 	}
 	std::cout << std::endl;
 }
-
-// int main(int argc, char **argv)
-// {
-// 	if (argc == 1)
-// 	{
-// 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-// 		return (0);
-// 	}
-// 	for (int i = 1; argv[i]; i++)
-// 	{
-// 		for (int j = 0; argv[i][j]; j++)
-// 			std::cout << (char)toupper(argv[i][j]);
-// 		// if (argv[i + 1])
-// 		// 	std::cout << " ";
-// 	}
-// 	std::cout << std::endl;
-
-
-
-// }
-
-void a(char)
-{
-
-}
-void a(int)
-{
-
-}
-void a(int, int)
-{
-
-}
+*/
