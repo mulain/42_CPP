324a6ba793855675d35573cf58f8feabc60b26c9
Wolf Arif Mardin <wolf.mardin@gmail.com>
2023-01-05 22:24:16 +0100

    auto add & push

--- CPP01/ex00/Zombie.cpp
+++ CPP01/ex00/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: wmardin <wmardin@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 20:45:57 by wmardin           #+#    #+#             */
-/*   Updated: 2023/01/05 21:33:53 by wmardin          ###   ########.fr       */
+/*   Updated: 2023/01/05 22:24:14 by wmardin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,16 +27,3 @@ void	Zombie::announce(void)
 {
 	std::cout << this->_name << ": BraiiiiiiinnnzzzZ..." << std::endl;
 }
-
- /*
-Then, implement the two following functions:
-• Zombie* newZombie( std::string name );
-It creates a zombie, name it, and return it so you can use it outside of the function
-scope.
-
-• void randomChump( std::string name );
-It creates a zombie, name it, and the zombie announces itself.
-Now, what is the actual point of the exercise? You have to determine in what case
-it’s better to allocate the zombies on the stack or heap.
-
-*/
