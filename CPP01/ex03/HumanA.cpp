c887e4637ee4a1ae34233bd1efef3e317c65c75e
Wolf Arif Mardin <wolf.mardin@gmail.com>
2023-01-07 22:52:23 +0100

    auto add & push

--- CPP01/ex03/HumanA.cpp
+++ CPP01/ex03/HumanA.cpp
@@ -6,23 +6,25 @@
 /*   By: wmardin <wmardin@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/07 22:32:17 by wmardin           #+#    #+#             */
-/*   Updated: 2023/01/07 22:43:22 by wmardin          ###   ########.fr       */
+/*   Updated: 2023/01/07 22:52:10 by wmardin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanA.hpp"
 
-HumanA::HumanA(void)
+HumanA::HumanA(std::string name, Weapon weapon):
+	_name(name),
+	_weapon(weapon)
 {
-
+	std::cout << this->_name << " has spawned." << std::endl;
 }
 
 HumanA::~HumanA(void)
 {
-
+	std::cout << this->_name << " has left the arena." << std::endl;
 }
 
 void	HumanA::attack(void)
 {
-	std::cout << this->_name << " attacks with their " << this->_weapon << std::endl;
+	std::cout << this->_name << " attacks with their " << this->_weapon.getType() << std::endl;
 }
--- CPP01/ex03/HumanA.hpp
+++ CPP01/ex03/HumanA.hpp
@@ -6,7 +6,7 @@
 /*   By: wmardin <wmardin@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/07 22:33:13 by wmardin           #+#    #+#             */
-/*   Updated: 2023/01/07 22:41:32 by wmardin          ###   ########.fr       */
+/*   Updated: 2023/01/07 22:49:25 by wmardin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,19 @@
 #define HumanA_H
 
 #include <iostream>
+#include "Weapon.hpp"
 
 class HumanA
 {
 	public:
-		HumanA(void);
+		HumanA(std::string, Weapon);
 		~HumanA(void);
 
 		void	HumanA::attack(void);
 
 	private:
-		std::string		_weapon;
 		std::string		_name;
+		Weapon			_weapon;
 };
 
 #endif
