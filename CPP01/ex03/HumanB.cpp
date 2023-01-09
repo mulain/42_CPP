f30d5b01f3795222b407ac5da7e52761011f8374
Wolf Mardin <wmardin@c2s15d88.42wolfsburg.de>
2023-01-09 18:04:34 +0100

    auto add & push

--- CPP01/ex03/HumanB.cpp
+++ CPP01/ex03/HumanB.cpp
@@ -6,16 +6,17 @@
 /*   By: wmardin <wmardin@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 13:59:13 by wmardin           #+#    #+#             */
-/*   Updated: 2023/01/09 17:51:06 by wmardin          ###   ########.fr       */
+/*   Updated: 2023/01/09 18:02:18 by wmardin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanB.hpp"
 
 HumanB::HumanB(std::string name):
-	_name(name)
+	_name(name),
+	_weapon(NULL)
 {
-		std::cout << this->_name << " has spawned." << std::endl;
+	std::cout << this->_name << " has spawned." << std::endl;
 }
 
 HumanB::~HumanB(void)
@@ -26,7 +27,9 @@ HumanB::~HumanB(void)
 void	HumanB::attack(void)
 {
 	if (this->_weapon)
-		std::cout << this->_name << " attacks with their " << this->_weapon->getType() << std::endl;
+		std::cout << this->_name << " attacks with their " << this->_weapon->getType() << '.' << std::endl;
+	else
+		std::cout << this->_name << " just flails around weaponlessly."
 }
 
 void	HumanB::setWeapon(Weapon weapon)
