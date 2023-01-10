42305ff2e461d60a6dd9881d7b196bbea364d8ca
Wolf Mardin <wmardin@c1s7d33.42wolfsburg.de>
2023-01-10 14:41:26 +0100

    auto add & push

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -6,11 +6,12 @@
 /*   By: wmardin <wmardin@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 18:05:13 by wmardin           #+#    #+#             */
-/*   Updated: 2023/01/09 18:13:51 by wmardin          ###   ########.fr       */
+/*   Updated: 2023/01/10 14:40:06 by wmardin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <fstream>
 
 #define E_ARGCOUNT	"Wrong number of arguments. Please provide <filename>, <string1>, <string2>"
 
@@ -23,17 +24,23 @@ Using C file manipulation functions is forbidden and will be considered cheating
 All the member functions of the class std::string are allowed, except replace. Use them wisely!
 Of course, handle unexpected inputs and errors.
 You have to create and turn in your own tests to ensure your program works as expected
+
+forbidden: std::string::replace
 */
 
 int	main(int argc, char **argv)
 {
 	std::string		filename;
+	std::string		outfile;
 
+	//if (argc != 4 || !argv[2][0] || !argv[3][0]) Why this needed?
 	if (argc != 4)
 	{
 		std::cout << E_ARGCOUNT << std::endl;
 		return (1);
 	}
 	filename = argv[1];
-	if (filename.)
+	outfile = argv[1];
+	outfile.append(".replace");
+	//if (filename.)
 }
