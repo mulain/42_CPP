3c40911ceace24a49f7329397f7c368e85caf581
Wolf Mardin <wmardin@c2s3d8.42wolfsburg.de>
2023-01-10 15:51:57 +0100

    auto add & push

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -6,14 +6,17 @@
 /*   By: wmardin <wmardin@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 18:05:13 by wmardin           #+#    #+#             */
-/*   Updated: 2023/01/10 14:40:06 by wmardin          ###   ########.fr       */
+/*   Updated: 2023/01/10 15:50:55 by wmardin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
 
-#define E_ARGCOUNT	"Wrong number of arguments. Please provide <filename>, <string1>, <string2>"
+#define E_ARGCOUNT		"Wrong number of arguments. Please provide <filename>, <string1>, <string2>"
+#define E_INVALIDARG	"<string1> and <string2> may not be empty."
+#define E_INFILE		"Infile error."
+#define E_OUTFILE		"Outfile error. "
 
 /*
 Create a program that takes three parameters in the following order:
@@ -30,17 +33,40 @@ forbidden: std::string::replace
 
 int	main(int argc, char **argv)
 {
-	std::string		filename;
-	std::string		outfile;
+	std::string		outfile_name;
+	std::string		buffer;
+	std::string		content;
+	std::ifstream	infile;
+	std::ofstream	outfile;
 
-	//if (argc != 4 || !argv[2][0] || !argv[3][0]) Why this needed?
 	if (argc != 4)
 	{
 		std::cout << E_ARGCOUNT << std::endl;
 		return (1);
 	}
-	filename = argv[1];
-	outfile = argv[1];
-	outfile.append(".replace");
-	//if (filename.)
+	if (!argv[2][0] || !argv[3][0]) //wrong. argv2.empty should just copy the file and argv3.empty should delete argv2 occurences
+	{
+		std::cout << E_INVALIDARG << std::endl;
+		return (1);
+	}
+	infile.open(argv[1]);
+	if (!infile.is_open())
+	{
+		std::cout << E_INFILE << std::endl;
+		return (1);
+	}
+	outfile_name = argv[1];
+	outfile_name.append(".replace");
+	outfile.open(outfile_name);
+	if (!outfile.is_open())
+	{
+		std::cout << E_OUTFILE << std::endl;
+		return (1);
+	}
+	while (std::getline(infile, buffer))
+		content.append(buffer + '\n');
+	content.pop_back();
+	infile.close();
+
+
 }
