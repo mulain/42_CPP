a09f8b2a49e8764b461ead9330fa3c9c27f9e8b5
Wolf Mardin <wmardin@c1s7d33.42wolfsburg.de>
2023-01-10 14:25:08 +0100

    auto add & push

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -6,27 +6,11 @@
 /*   By: wmardin <wmardin@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 16:45:22 by wmardin           #+#    #+#             */
-/*   Updated: 2023/01/06 17:12:23 by wmardin          ###   ########.fr       */
+/*   Updated: 2023/01/09 13:40:30 by wmardin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <string>
 #include <iostream>
-
-/*
-Write a program that contains:
-• A string variable initialized to "HI THIS IS BRAIN".
-• stringPTR: A pointer to the string.
-• stringREF: A reference to the string.
-Your program has to print:
-• The memory address of the string variable.
-• The memory address held by stringPTR.
-• The memory address held by stringREF.
-And then:
-• The value of the string variable.
-• The value pointed to by stringPTR.
-• The value pointed to by stringREF
-*/
 
 int	main(void)
 {
