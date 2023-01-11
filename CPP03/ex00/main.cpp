c73ab690c51892c551de60970b614f600eb6b3d0
Wolf Mardin <wmardin@c2s3d8.42wolfsburg.de>
2023-01-11 19:57:15 +0100

    auto add & push

--- CPP03/ex00/main.cpp
+++ CPP03/ex00/main.cpp
@@ -1,38 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wmardin <wmardin@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/11 19:41:04 by wmardin           #+#    #+#             */
-/*   Updated: 2023/01/11 19:56:29 by wmardin          ###   ########.fr       */
+/*   Created: 2023/01/11 19:56:52 by wmardin           #+#    #+#             */
+/*   Updated: 2023/01/11 19:57:09 by wmardin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
-ClapTrap::ClapTrap(std::string name): _name(name), _hitpoints(10), _energypoints(10), _attackdmg(0)
-{
-	std::cout << "ClapTrap " << name << " created." << std::endl;
-}
-
-ClapTrap::~ClapTrap(void)
-{
-	std::cout << "ClapTrap " << name << " destroyed." << std::endl;
-}
-
-ClapTrap::attack(const std::string &target)
-{
-
-}
-
-ClapTrap::takeDamage(unsigned int amount)
-{
-
-}
-
-ClapTrap::beRepaired(unsigned int amount)
+int	main(void)
 {
 
 }
