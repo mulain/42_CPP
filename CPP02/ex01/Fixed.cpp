d3aea3fa7fa4d871376af777922c17b4493a6109
Wolf Mardin <wmardin@c2s3d8.42wolfsburg.de>
2023-01-11 12:26:44 +0100

    auto add & push

--- CPP02/ex01/Fixed.cpp
+++ CPP02/ex01/Fixed.cpp
@@ -6,8 +6,26 @@
 /*   By: wmardin <wmardin@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 11:45:46 by wmardin           #+#    #+#             */
-/*   Updated: 2023/01/11 11:46:05 by wmardin          ###   ########.fr       */
+/*   Updated: 2023/01/11 12:25:20 by wmardin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
+
+Fixed::Fixed(void):
+	_integer(0)
+{
+	std::cout << "Default constructor called." << std::endl;
+}
+
+Fixed::Fixed(const Fixed &copy)
+{
+	std::cout << "Copy constructor called." << std::endl;
+	*this = copy;
+}
+
+Fixed::~Fixed(void):
+	_integer(0)
+{
+	std::cout << "Default destructor called." << std::endl;
+}
--- CPP02/ex01/Fixed.hpp
+++ CPP02/ex01/Fixed.hpp
@@ -6,20 +6,39 @@
 /*   By: wmardin <wmardin@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 11:45:48 by wmardin           #+#    #+#             */
-/*   Updated: 2023/01/11 11:50:23 by wmardin          ###   ########.fr       */
+/*   Updated: 2023/01/11 12:20:42 by wmardin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
 #define FIXED_HPP
 
+#include <iostream>
+
 class Fixed
 {
 	public:
 		Fixed(void);
+		Fixed(const Fixed &);
 		~Fixed(void);
+		Fixed &operator=(const Fixed &);
 
 	private:
+		int					_integer;
+		static const int	frac_bits = 8;
 };
 
 #endif
+
+An integer to store the fixed-point number value.
+◦ A static constant integer to store the number of fractional bits. Its value
+will always be the integer literal 8.
+• Public members:
+◦ A default constructor that initializes the fixed-point number value to 0.
+◦ A copy constructor.
+◦ A copy assignment operator overload.
+◦ A destructor.
+◦ A member function int getRawBits( void ) const;
+that returns the raw value of the fixed-point value.
+◦ A member function void setRawBits( int const raw );
+that sets the raw value of the fixed-point number
