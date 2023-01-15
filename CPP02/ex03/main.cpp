3b4d71b54895b1274bdaf740fdd9f813cc12606b
wolf <wolf.mardin@gmail.com>
2023-01-15 17:27:33 +0100

    auto add & push

--- CPP02/ex03/main.cpp
+++ CPP02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: wmardin <wmardin@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 19:49:04 by wmardin           #+#    #+#             */
-/*   Updated: 2023/01/15 13:47:30 by wmardin          ###   ########.fr       */
+/*   Updated: 2023/01/15 17:22:26 by wmardin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@ int	main(void)
 	Point const c("c", 3, 3);
 	Point const p1("p1", 3, 1.5);		//inside
 	Point const p2("p2", 2, 1);			//on hypotenuse (edge)
-	Point const p3("p3", 6.5, 4.5);		//outside
-	Point const p4("p4", 0.99, 3); 		//outside but with small fractional difference
+	Point const p3("p3", 4, 3);		//outside
+	Point const p4("p4", 3, 0.99); 		//outside but with small fractional difference
 	Point const p5("p5", 3, 3); 		//vertex c
 	std::cout << "Triangle: " << a << b << c << std::endl;
 	std::cout << "Point " << p1 << (bsp(a, b, c, p1) ? " is " : " is NOT ") << "inside the triangle" << std::endl;
