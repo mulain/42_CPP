022b13ce393f727a3d4bfb218c776f0d4d6da4d8
Wolf Arif Mardin <wolf.mardin@gmail.com>
2023-01-16 11:32:35 +0100

    auto add & push

--- CPP03/ex02/FragTrap.cpp
+++ CPP03/ex02/FragTrap.cpp
@@ -6,7 +6,47 @@
 /*   By: wmardin <wmardin@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 19:53:22 by wmardin           #+#    #+#             */
-/*   Updated: 2023/01/15 19:53:22 by wmardin          ###   ########.fr       */
+/*   Updated: 2023/01/16 11:32:05 by wmardin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "FragTrap.hpp"
+
+FragTrap::FragTrap(void)
+{
+	_hitpoints = 100;
+	_energypoints = 100;
+	_attackdmg = 30;
+	std::cout << "Unnamed FragTrap created." << std::endl;
+}
+
+FragTrap::FragTrap(std::string name)
+{
+	_name = name;
+	_hitpoints = 100;
+	_energypoints = 100;
+	_attackdmg = 30;
+	std::cout << "FragTrap " << _name << " created." << std::endl;
+}
+
+FragTrap::FragTrap(FragTrap const & rhs): ClapTrap()
+{
+	*this = rhs;
+	std::cout << "FragTrap copy constructor called." << std::endl;
+}
+
+FragTrap & FragTrap::operator=(FragTrap const & rhs)
+{
+	this->_name = rhs._name + "_copy";
+	this->_attackdmg = rhs._attackdmg;
+	this->_energypoints = rhs._energypoints;
+	this->_hitpoints = rhs._hitpoints;
+	return (*this);
+}
+
+void	FragTrap::highFivesGuys(void)
+{
+	if (!isActive())
+		return;
+	std::cout << "FragTrap " << _name << " is in a good mood and wants to high five!" << std::endl;
+}
--- CPP03/ex02/FragTrap.hpp
+++ CPP03/ex02/FragTrap.hpp
@@ -6,7 +6,26 @@
 /*   By: wmardin <wmardin@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 19:53:25 by wmardin           #+#    #+#             */
-/*   Updated: 2023/01/15 19:53:26 by wmardin          ###   ########.fr       */
+/*   Updated: 2023/01/16 11:25:18 by wmardin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FRAGTRAP_HPP
+#define FRAGTRAP_HPP
+
+#include "ClapTrap.hpp"
+
+class FragTrap: public ClapTrap
+{
+	public:
+		FragTrap(void);
+		FragTrap(std::string);
+		FragTrap(FragTrap const &);
+		~FragTrap(void);
+
+		FragTrap	&operator=(FragTrap const &);
+
+		void	highFivesGuys(void);
+};
+
+#endif
