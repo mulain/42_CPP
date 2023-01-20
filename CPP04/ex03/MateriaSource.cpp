e2841b8ee9f0d3e68c5105b618177ebfddef944d
Wolf Mardin <wmardin@c2s15d88.42wolfsburg.de>
2023-01-20 19:10:24 +0100

    auto add & push

---
+++ CPP04/ex03/MateriaSource.cpp
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MateriaSource.cpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wmardin <wmardin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/20 18:40:07 by wmardin           #+#    #+#             */
+/*   Updated: 2023/01/20 19:08:41 by wmardin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MateriaSource.hpp"
+
+MateriaSource::MateriaSource(void)
+{
+	for (int i = 0; i < 4; i++)
+		_inventory[i] = NULL;
+}
+
+MateriaSource::~MateriaSource(void)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		if (this->_inventoryi[i])
+			delete this->_inventoryi];
+	}
+}
+
+MateriaSource::MateriaSource(MateriaSource const & src)
+{
+	*this = src;
+}
+
+MateriaSource & MateriaSource::operator=(MateriaSource const & src)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		if (_inventory[i])
+			delete _inventory[i];
+		if (src._inventory[i])
+			_inventory[i] = src._inventory[i]->clone();
+		else
+			_inventory[i] = NULL;
+	}
+	return *this;
+}
+
+void MateriaSource::learnMateria(AMateria *materia)
+{
+	//guard vs NULL? prolly not necessary...
+	int i = 0;
+	
+	while (i < 4 && _inventory[i])
+		i++;
+	if (i == 4)
+		std::cout "Inventory is full!" << std::endl;
+	else
+		_inventory[i] = materia->clone();		
+}
---
+++ CPP04/ex03/MateriaSource.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MateriaSource.hpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wmardin <wmardin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/20 18:40:33 by wmardin           #+#    #+#             */
+/*   Updated: 2023/01/20 18:48:24 by wmardin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MATERIASOURCE_HPP
+#define MATERIASOURCE_HPP
+
+#include <iostream>
+#include "IMateriaSource.hpp"
+#include "AMateria.hpp"
+
+class MateriaSource: public IMateriaSource
+{
+	public:
+		MateriaSource(void);
+		~MateriaSource(void);
+		MateriaSource(MateriaSource const &);
+		
+		MateriaSource & operator=(MateriaSource const &);
+		
+		void learnMateria(AMateria *);
+		AMateria* createMateria(std::string const &);
+	
+	private:
+		AMateria * _inventory[4];
+}
