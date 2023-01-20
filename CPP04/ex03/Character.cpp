41e052e78e8d926c2a8bbe2f8ca9372b3ae71495
wolf <wolf.mardin@gmail.com>
2023-01-20 15:36:11 +0100

    auto add & push

---
+++ CPP04/ex03/Character.cpp
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Character.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wmardin <wmardin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/20 11:44:24 by wmardin           #+#    #+#             */
+/*   Updated: 2023/01/20 15:19:01 by wmardin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Character.hpp"
+
+Character::Character(void):
+	_name("Unnamed")
+{
+	for (int i = 0; i < 4; i++)
+		_inventory[i] = NULL;
+}
+
+Character::~Character(void)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		if (_inventory[i])
+			delete _inventory[i];
+	}
+}
+
+Character::Character(std::string name):
+	_name(name)
+{
+	for (int i = 0; i < 4; i++)
+		_inventory[i] = NULL;
+}
+
+Character::Character(Character const & src)
+{
+	*this = src;
+}
+
+Character & Character::operator=(Character const & src)
+{
+	_name = src._name + "_copy";
+	for (int i= 0; i < 4; i++)
+	{
+		if (src._inventory[i])
+			_inventory[i] = src._inventory[i]->clone();
+		else
+			_inventory[i] = NULL;
+	}
+	return *this;
+}
+
+std::string const & Character::getName(void) const
+{
+	return _name;
+}
+
+void Character::equip(AMateria* m)
+{
+	if (!m)
+	{
+		std::cout << "Cannot equip materia of type NULL!" << std::endl;
+		return;
+	}
+	int i = 0;
+	while (i < 4 && _inventory[i])
+		i++;
+	if (i == 4)
+		std::cout << "Inventory is full, cannot equip new materia!" << std::endl;
+	else
+		_inventory[i] = m;
+ }
+
+ void Character::unequip(int idx)
+ {
+	if (!_inventory[idx])
+		std::cout << "Inventory slot is already empty!" << std::endl;
+	else
+		_inventory[idx] = NULL;
+ }
+
+ void Character::use(int idx, ICharacter & target)
+ {
+	if (idx < 0 || idx > 3)
+		std::cout << "Invalid inventory slot!" << std::endl;
+	else if (!_inventory[idx])
+		std::cout << "Inventory slot is empty!" << std::endl;
+	else
+		_inventory[idx]->use(target);
+ }
---
+++ CPP04/ex03/Character.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Character.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wmardin <wmardin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/20 11:37:18 by wmardin           #+#    #+#             */
+/*   Updated: 2023/01/20 11:44:10 by wmardin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CHARACTER_HPP
+#define CHARACTER_HPP
+
+#include <iostream>
+#include  "AMateria.hpp"
+
+class Character: public ICharacter
+{
+	public:
+		Character(void);
+		~Character(void);
+		Character(std::string const);
+		Character(Character const &);
+
+		Character & operator=(Character const &);
+
+		std::string const & getName(void) const;
+		//Materiagetter: 	AMateria *getMateriaAddr(int index) const;
+		void equip(AMateria* m);
+		void unequip(int idx);
+		void use(int idx, ICharacter& target);
+	
+	private:
+		std::string _name;
+		AMateria * _inventory[4];
+};
+
+#endif
