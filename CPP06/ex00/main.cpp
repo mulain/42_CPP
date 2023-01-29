e23db6f061f5a3f6d47ea98a1de2a6ec94bb4977
wolf <wolf.mardin@gmail.com>
2023-01-29 18:03:04 +0100

    auto add & push

--- CPP06/ex00/main.cpp
+++ CPP06/ex00/main.cpp
@@ -6,12 +6,26 @@
 /*   By: wmardin <wmardin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/29 16:00:23 by wmardin           #+#    #+#             */
-/*   Updated: 2023/01/29 17:13:53 by wmardin          ###   ########.fr       */
+/*   Updated: 2023/01/29 17:46:21 by wmardin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cstdlib>
+
+void argcountCheck(int argc)
+{
+	if (argc != 2)
+	{
+		std::cout << "Please enter exactly one argument!" << std::endl;
+		exit (1);
+	}
+}
+
+void formatCheck(std::string input)
+{
+	
+}
 
 void handlePseudo(std::string input)
 {
@@ -28,17 +42,13 @@ void handlePseudo(std::string input)
 		std::cout << "double: " << ((i % 2) ? (input.substr(0, input.size() - 1)) : input) << std::endl;
 		exit (0);
 	}
-	
-	
 }
 
 int main (int argc, char** argv)
 {
-	if (argc != 2)
-	{
-		std::cout << "Please enter exactly one argument!" << std::endl;
-		return (1);
-	}
+	argcountCheck(argc);
 	std::string input(argv[1]);
 	handlePseudo(input);
+	formatCheck(input);
+	std::cout << "Commencing rest" << std::endl;
 }
