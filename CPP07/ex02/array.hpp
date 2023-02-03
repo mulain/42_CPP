77c10e593d843eb66d832b1ea522b0178294cd71
wolf <wolf.mardin@gmail.com>
2023-02-03 17:58:20 +0100

    auto add & push

--- CPP07/ex02/array.hpp
+++ CPP07/ex02/array.hpp
@@ -6,7 +6,7 @@
 /*   By: wmardin <wmardin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 22:17:57 by wmardin           #+#    #+#             */
-/*   Updated: 2023/02/02 22:22:16 by wmardin          ###   ########.fr       */
+/*   Updated: 2023/02/03 17:55:55 by wmardin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,13 +38,80 @@ As usual, ensure everything works as expected and turn in a main.cpp file that c
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
+#include <iostream>
+#include <stdexcept>
+
 template <typename T>
 class Array
 {
 	public:
-		Array(void);
-		~Array(void);
+		// Constructors
+		Array(void):
+			_size(0),
+			_content(new T[_size])
+		{}
+		Array(unsigned int n):
+			_size(n),
+			_content(new T[_size])
+		{}
+		Array(const Array& src)
+		{
+			*this = src;
+		}
 		
+		// Destructors
+		~Array(void)
+		{
+			delete[] _content;
+		}
+		
+		// Operator overloads
+		Array& operator=(const Array& src)
+		{
+			_size = src._size;
+			delete[] _content;
+			_content = new T[_size];
+			for (size_t i = 0; i < _size; i++)
+				_content[i] = src._content[i];
+			return *this;			
+		}
+
+		T& operator[](size_t index)
+		{
+			checkIndex(index);
+			return _content[index];
+		}
+		
+		// Getters
+		size_t getSize(void)
+		{
+			return _size;
+		}
+
+		T getElement(size_t index)
+		{
+			checkIndex(index);
+			return _content[index];
+		}
+
+		// Setters
+		void setElement(size_t index, T value)
+		{
+			checkIndex(index);
+			_content[index] = value;
+		}
+	
+	private:
+		// Checkers
+		void checkIndex(size_t index)
+		{
+			if (index >= _size)
+				throw std::out_of_range("Invalid index.");
+		}
+		
+		// Attributes
+		size_t	_size;
+		T*		_content;
 };
 
 #endif
--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -6,42 +6,75 @@
 /*   By: wmardin <wmardin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 08:42:02 by wmardin           #+#    #+#             */
-/*   Updated: 2023/02/02 17:23:55 by wmardin          ###   ########.fr       */
+/*   Updated: 2023/02/03 17:57:48 by wmardin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-
-#include "iter.hpp"
+#include "Array.hpp"
 
 int main (void)
 {
 	std::string	partition(50, '-');
-	char		chars[5] = {'a', 'b', 'c', 'd', 0};
-	int			ints[5] = {2, 3, 5, 7, 11};
-	char*		charptrs[5];
-	int*		intptrs[5];
 
-	for (int i = 0; i < 5; i++)
+	std::cout << partition << std::endl;
+	std::cout << "Part 0: Automatic initialization when using <new> without arguments" << std::endl;
+	std::cout << partition << std::endl;
+	
+	int* a = new int();
+	std::cout << *a << std::endl;
+	
+	std::cout << partition << std::endl;
+	std::cout << "Part 1: Initializiation with and without arguments, then trying to print the array" << std::endl;
+	std::cout << partition << std::endl;
+	
+	Array<int>		array_a(5);
+	Array<char>		array_b;
+
+	std::cout << "array_a size: " << array_a.getSize() << std::endl;
+	try
 	{
-		charptrs[i] = &chars[i];
-		intptrs[i] = &ints[i];
+		for (int i = 0; i < 5; i++)
+		std::cout << "array_a element[" << i << "]: " << array_a.getElement(i) << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
 	}
 	
-	iter(chars, 5, print);
+	std::cout << "array_b size: " << array_b.getSize() << std::endl;
+	try
+	{
+		for (int i = 0; i < 5; i++)
+		std::cout << "array_b element[" << i << "]: " << array_b.getElement(i) << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	std::cout << partition << std::endl;
+	std::cout << "Part 2: Setting array_a element 1 to value of 42, then trying with array_b" << std::endl;
 	std::cout << partition << std::endl;
 	
-	iter(ints, 5, print);
-	std::cout << partition << std::endl;
+	array_a.setElement(1, 42);
+	try
+	{
+		for (int i = 0; i < 5; i++)
+		std::cout << "array_a element[" << i << "]: " << array_a.getElement(i) << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
 	
-	iter(charptrs, 5, print);
-	std::cout << partition << std::endl;
-	
-	iter(intptrs, 5, print);
-	std::cout << partition << std::endl;
-	
-	iter(ints, 5, increment);	
-	iter(ints, 5, print);
+	try
+	{
+		array_b.setElement(1, 42);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
 
 	return 0;
 }
