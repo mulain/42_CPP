f66051d55b07e5bc9f5a08c268e6f77cafa62c40
Wolf Mardin <wmardin@c2s4d10.42wolfsburg.de>
2023-02-04 16:53:53 +0100

    auto add & push

--- CPP08/ex01/main.cpp
+++ CPP08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: wmardin <wmardin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 08:42:02 by wmardin           #+#    #+#             */
-/*   Updated: 2023/02/04 12:17:55 by wmardin          ###   ########.fr       */
+/*   Updated: 2023/02/04 16:53:15 by wmardin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,21 @@ int main (void)
 	std::string	partition(70, '-');
 
 	std::cout << partition << std::endl;
-	std::cout << "Part 0: Instantiating Span objects" << std::endl;
+	std::cout << "Part 0: Subject test" << std::endl;
 	std::cout << partition << std::endl;
+ 	Span sp = Span(5);
+	sp.addNumber(6);
+	sp.addNumber(3);
+	sp.addNumber(17);
+	sp.addNumber(9);
+	sp.addNumber(11);
+	std::cout << sp.shortestSpan() << std::endl;
+	std::cout << sp.longestSpan() << std::endl;
 
+	std::cout << partition << std::endl;
+	std::cout << "Part 1: Empty span object" << std::endl;
+	std::cout << partition << std::endl;
 	Span	span1;
-	Span	spanbigboiii(10000);
-
-	std::cout << partition << std::endl;
-	std::cout << "Part 1: Empty span" << std::endl;
-std::cout << partition << std::endl;
-
 	try
 	{
 		std::cout << "Longest span of span1: " << span1.longestSpan() << std::endl;
@@ -47,7 +52,6 @@ std::cout << partition << std::endl;
 	std::cout << partition << std::endl;
 	std::cout << "Part 2: Small span object with overflow" << std::endl;
 	std::cout << partition << std::endl;
-	
 	for (int i = 0; i < 12; i++)
 	{
 		try
@@ -67,10 +71,12 @@ std::cout << partition << std::endl;
 		catch(const std::exception& e)
 		{
 			std::cerr << e.what() << std::endl;
-		}
-		
-		
-		
+		}		
 	}
+	std::cout << partition << std::endl;
+	std::cout << "Part 3: Big span" << std::endl;
+	std::cout << partition << std::endl;
+
+
 	return 0;
 }
