e4d38fc58a3095595e2f703b678be963f582a2d7
Wolf Arif Mardin <wolf.mardin@gmail.com>
2023-02-05 18:00:53 +0100

    auto add & push

--- CPP07/ex02/Array.hpp
+++ CPP07/ex02/Array.hpp
@@ -1,29 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   array.hpp                                          :+:      :+:    :+:   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wmardin <wmardin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 22:17:57 by wmardin           #+#    #+#             */
-/*   Updated: 2023/02/04 18:51:43 by wmardin          ###   ########.fr       */
+/*   Updated: 2023/02/05 18:00:41 by wmardin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-/*
-
-• Construction by copy and assignment operator. In both cases, modifying either the
-original array or its copy after copying musn’t affect the other array.
-• You MUST use the operator new[] to allocate memory. Preventive allocation (allocating memory in advance) is forbidden. Your program must never access nonallocated memory.
-• Elements can be accessed through the subscript operator: [ ].
-• When accessing an element with the [ ] operator, if its index is out of bounds, an
-std::exception is thrown.
-• A member function size() that returns the number of elements in the array. This
-member function takes no parameter and musn’t modify the current instance.
-As usual, ensure everything works as expected and turn in a main.cpp file that contains your tests.
-8
-
-*/
 
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
