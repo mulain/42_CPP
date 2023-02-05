5ed3d695f4e968b324023ad6ce6bd337ec329e17
Wolf Mardin <wmardin@c2s15d88.42wolfsburg.de>
2023-02-05 14:06:22 +0100

    auto add & push

---
+++ CPP08/ex02/MutantStack.hpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wmardin <wmardin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/05 13:36:37 by wmardin           #+#    #+#             */
+/*   Updated: 2023/02/05 13:58:51 by wmardin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANT_HPP
+#define MUTANT_HPP
+
+#include <stack>
+
+template <typename T>
+class MutantStack: public std::stack<T>
+{
+	public:
+		MutantStack(void) {}
+		~MutantStack(void) {}
+		MutantStack(const MutantStack& src)
+		{
+			*this = src;	
+		}
+		MutantStack& operator=(const MutantStack& src)
+		{
+			std::stack<T>::operator=(src);
+			return *this;
+		}
+		
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		iterator begin(void)
+		{
+			return std::stack<T>::begin();
+		}
+		
+		iterator end(void)
+		{
+			return std::stack<T>::end();
+		}
+};
+
+#endif
