a0ffff6531ac69b731019624f75676c47a289d7a
Wolf Mardin <wmardin@c2s15d88.42wolfsburg.de>
2023-02-05 12:07:39 +0100

    auto add & push

--- CPP08/ex00/easyfind.hpp
+++ CPP08/ex00/easyfind.hpp
@@ -6,38 +6,19 @@
 /*   By: wmardin <wmardin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/03 21:56:19 by wmardin           #+#    #+#             */
-/*   Updated: 2023/02/04 10:20:19 by wmardin          ###   ########.fr       */
+/*   Updated: 2023/02/05 12:04:31 by wmardin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-/*
-A first easy exercise is the way to start off on the right foot.
-Write a function template easyfind that accepts a type T. It takes two parameters.
-The first one has type T and the second one is an integer.
-Assuming T is a container of integers, this function has to find the first occurrence
-of the second parameter in the first parameter.
-If no occurrence is found, you can either throw an exception or return an error value
-of your choice. If you need some inspiration, analyze how standard containers behave.
-Of course, implement and turn in your own tests to ensure everything works as expected.
-You don’t have to handle associative containers.
-6
-*/
 
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
-#include <iostream>
 #include <algorithm>
 
 template<typename T>
-typename T::iterator easyFind(T container, int i)
+typename T::iterator easyFind(T& container, int value)
 {
-	typename T::iterator iter;
-
-	iter = find(container.begin(), container.end(), i);
-	if (iter == container.end())
-		throw std::runtime_error("Value not found.");
-	return iter;
+	return std::find(container.begin(), container.end(), value);
 }
 
 #endif
--- CPP08/ex00/main.cpp
+++ CPP08/ex00/main.cpp
@@ -6,46 +6,37 @@
 /*   By: wmardin <wmardin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 08:42:02 by wmardin           #+#    #+#             */
-/*   Updated: 2023/02/04 16:50:38 by wmardin          ###   ########.fr       */
+/*   Updated: 2023/02/05 12:07:08 by wmardin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <vector>
-#include <list>
+#include <iostream>
 
 #include "easyfind.hpp"
 
+#define VALNOTFOUND "Value not found!"
+
 int main (void)
 {
-	std::vector<int>	vector1;
-	std::string	partition(70, '-');
+	std::vector<int>			vector1;
+	std::vector<int>::iterator	iter;
+	std::string					partition(70, '-');
 
 	std::cout << partition << std::endl;
 	std::cout << "Part 1: int vector: value that is present" << std::endl;
 	std::cout << partition << std::endl;
 	for (int i = 1; i < 11; i++)
 		vector1.push_back(i);
-	try
-	{
-		std::cout << *easyFind(vector1, 1) << std::endl;
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
-
+	
+	iter = easyFind(vector1, 2);
+	std::cout << (iter != vector1.end() ? "Value found!" : "Value not found!") << std::endl;
+	
 	std::cout << partition << std::endl;
 	std::cout << "Part 2: int vector: value that is not present" << std::endl;
 	std::cout << partition << std::endl;
-	for (int i = 1; i < 11; i++)
-		vector1.push_back(i);
-	try
-	{
-		std::cout << *easyFind(vector1, 42) << std::endl;
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
+	
+	iter = easyFind(vector1, 42);
+	std::cout << (iter != vector1.end() ? "Value found!" : "Value not found!") << std::endl;
 	return 0;
 }
