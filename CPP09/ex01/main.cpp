1ddbeeaadf5ab687075e160f8fa991c9dec07381
Wolf Arif Mardin <wolf.mardin@gmail.com>
2023-03-19 02:14:38 +0100

    auto add & push

--- CPP09/ex01/main.cpp
+++ CPP09/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: wmardin <wmardin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 19:32:46 by wmardin           #+#    #+#             */
-/*   Updated: 2023/03/18 13:47:21 by wmardin          ###   ########.fr       */
+/*   Updated: 2023/03/19 02:13:18 by wmardin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,54 +18,89 @@ You must create a program with these constraints:
 • Your program must process this expression and output the correct result on the standard output.
 • If an error occurs during the execution of the program an error message should be displayed on the standard output.
 • Your program must be able to handle operations with these tokens: "+ - / *".
+
+For example, the infix expression (3 × 4) + (5 × 6) becomes 3 4 × 5 6 × + in reverse Polish notation.
+3456
+xx+
+
 */
 
+#include <iostream>
+#include <stack>
+
+std::string syntaxCheck(int argc, char **argv)
+{
+	std::string input;
+	
+	if (argc < 2)
+	{
+		std::cout << "Please provide an argument." << std::endl;
+		exit(1);
+	}
+	input = argv[1];
+	if (input.empty())
+	{
+		std::cout << "Nothing to do..." << std::endl;
+		exit(0);
+	}
+	/* for (size_t i = 0; i < input.length(); i++)
+	{
+		if (input[i] == ' ')
+		{
+			input.erase(i);
+			i--;
+		}
+	} */
+	if (input.find_first_not_of("0123456789+-/* ") != std::string::npos)
+	{
+		std::cout << "Illegal character detected: " << input.find_first_not_of("0123456789+-/*") << std::endl;
+		exit(1);
+	}
+	return input;
+}
+
+void performOperation(std::stack<int> *numbers, char operand)
+{
+	if (numbers->size() < 2)
+	{
+		std::cout << "Found less than 2 numbers to perform on for operand: " << operand << std::endl;
+		exit(1);
+	}
+	int result = numbers->top();
+	numbers->pop();
+	if (operand == '+')
+		result += numbers->top();
+	else if (operand == '-')
+		result -= numbers->top();
+	else if (operand == '*')
+		result *= numbers->top();
+	else if (operand == '/')
+		result /= numbers->top();
+	numbers->pop();
+	std::cout << result << std::endl;
+	numbers->push(result);
+}
 
 int main (int argc, char** argv)
 {
-	std::string partition(70, '-');
+	std::string input = syntaxCheck(argc, argv);
+	std::stack<int> numbers;
+	std::stack<int> operands;
 
-	if (argc < 2)
+	for (int i = 0; input[i]; i++)
 	{
-		std::cout << "Please provide an argument!" << std::endl;
-		exit(1);
+		if (input[i] == ' ')
+			continue;
+		else if (isdigit(input[i]))
+		{
+			if (numbers.size() > 1)
+				return std::cout << "Expected operand but found end of input." << std::endl, 1;
+			numbers.push(input[i] - 48);
+		}
+		else
+			performOperation(&numbers, input[i]);
 	}
-	{
-		std::cout << partition << std::endl;
-		std::cout << "Scope 0: Date range tests" << std::endl;
-		std::cout << partition << std::endl;
-	
-		Date date0(2008, 1, 1);
-		Date date1(2025, 2, 28);
-		Date date2(2020, 1, 1);
-		Date date3(2020, 1, 2);
-		Date date4(1900, 1, 1);
-		BitcoinExchange	mtgox("mtgox");
-
-		mtgox.importPriceHistory((char*)"data.csv");
-		mtgox.importAccountFile(argv[1]);
-		std::cout << date4 << ": " << mtgox.getPriceOnDate(date4) << std::endl;
-		std::cout << date0 << ": " << mtgox.getPriceOnDate(date0) << std::endl;
-		std::cout << date2 << ": " << mtgox.getPriceOnDate(date2) << std::endl;
-		std::cout << date3 << ": " << mtgox.getPriceOnDate(date3) << std::endl;
-		std::cout << date1 << ": " << mtgox.getPriceOnDate(date1) << std::endl;
-
-		BitcoinExchange poloniex("poloniex");
-
-		poloniex.printAccountOverview();
-		poloniex = mtgox;
-		poloniex.printAccountOverview();
-	}
-	{
-		std::cout << partition << std::endl;
-		std::cout << "Scope 1: shmismshmang" << std::endl;
-		std::cout << partition << std::endl;
-
-		BitcoinExchange	mtgox("mtgox");
-
-		mtgox.importPriceHistory((char*)"data.csv");
-		mtgox.importAccountFile(argv[1]);
-		mtgox.printAccountOverview();
-	}
-	
+	if (numbers.size() > 1)
+		std::cout << "Expected operand but found end of input." << std::endl;
+	return std::cout << numbers.top() << std::endl, 0;
 }
