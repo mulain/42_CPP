b903eeae49bd6ba87bc5ef5314dc9746f65a9465
wolf <wolf.mardin@gmail.com>
2023-03-20 21:48:06 +0100

    auto add & push

--- CPP09/ex01/main.cpp
+++ CPP09/ex01/main.cpp
@@ -6,33 +6,33 @@
 /*   By: wmardin <wmardin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 19:32:46 by wmardin           #+#    #+#             */
-/*   Updated: 2023/03/19 10:32:17 by wmardin          ###   ########.fr       */
+/*   Updated: 2023/03/20 17:07:20 by wmardin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <stack>
+#include <cstdlib>
+#include <sstream>
+
+void myExit(std::string msg, int exitcode)
+{
+	if (!msg.empty())
+		std::cout << msg << std::endl;
+	exit(exitcode);
+}
 
 std::string syntaxCheck(int argc, char **argv)
 {
-	std::string input;
-	
 	if (argc < 2)
-	{
-		std::cout << "Please provide an argument." << std::endl;
-		exit(1);
-	}
-	input = argv[1];
+		myExit("Please provide an argument." , 1);
+	std::string input = argv[1];
+	for (size_t i = input.find(" "); i != std::string::npos; i = input.find(" "))
+		input.erase(i, 1);
 	if (input.empty())
-	{
-		std::cout << "Nothing to do..." << std::endl;
-		exit(0);
-	}
-	if (input.find_first_not_of("0123456789+-/* ") != std::string::npos)
-	{
-		std::cout << "Illegal character detected." << std::endl;
-		exit(1);
-	}
+		myExit("Nothing to do..." , 0);
+	if (input.find_first_not_of("0123456789+-/*") != std::string::npos)
+		myExit("Illegal character detected." , 1);
 	return input;
 }
 
@@ -41,7 +41,8 @@ void performOperation(std::stack<int> *numbers, char operand)
 	if (numbers->size() < 2)
 	{
 		std::cout << "Found less than 2 numbers to perform on for operand: " << operand << std::endl;
-		std::cout << "Current result is: " << numbers->top() << std::endl;
+		if (!numbers->empty())
+			std::cout << "Current result is: " << numbers->top() << std::endl;
 		exit(1);
 	}
 	int rhs = numbers->top();
@@ -63,14 +64,14 @@ int main (int argc, char** argv)
 
 	for (int i = 0; input[i]; i++)
 	{
-		if (input[i] == ' ')
-			continue;
-		else if (isdigit(input[i]))
+		if (isdigit(input[i]))
 			numbers.push(input[i] - 48);
 		else
 			performOperation(&numbers, input[i]);
 	}
 	if (numbers.size() > 1)
 		std::cout << "Expected operand but found end of input." << std::endl << "Current result is: ";
-	return std::cout << numbers.top() << std::endl, 0;
+	
+	std::cout << numbers.top() << std::endl;
+	return 0;
 }
