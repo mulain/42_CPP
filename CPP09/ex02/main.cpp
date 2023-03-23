d3f1b5e24db6942e2f9d7771eb99ac9c6189f43f
wolf <wolf.mardin@gmail.com>
2023-03-23 09:21:49 +0100

    auto add & push

--- CPP09/ex02/main.cpp
+++ CPP09/ex02/main.cpp
@@ -11,47 +11,18 @@
 /* ************************************************************************** */
 
 /*
-• Your program must be able to use a positive integer sequence as argument.
-• Your program must use a merge-insert sort algorithm to sort the positive integer sequence.
-• If an error occurs during program execution, an error message should be displayed on the standard output.
-You must use at least two different containers in your code to
-validate this exercise.  Your program must be able to handle at
-least 3000 different integers.
+OSX:
+./PmergeMe `jot -r 3000 1 100000 | tr '\n' ' '`
 
-Here are some additional guidelines on the information you should display line by line on the standard output
-• On the first line you must display an explicit text followed by the unsorted positive integer sequence.
-• On the second line you must display an explicit text followed by the sorted positive integer sequence.
-• On the third line you must display an explicit text indicating the time used by your algorithm by specifying
-the first container used to sort the positive integer sequence.
-• On the last line you must display an explicit text indicating the time used by your algorithm by specifying
-the second container used to sort the positive integer sequence.
-
-$> ./PmergeMe 3 5 9 7 4
-Before: 3 5 9 7 4
-After: 3 4 5 7 9
-Time to process a range of 5 elements with std::[..] : 0.00031 us
-Time to process a range of 5 elements with std::[..] : 0.00014 us
-$> ./PmergeMe `shuf -i 1-100000 -n 3000 | tr "\n" " "`
-Before: 141 79 526 321 [...]
-After: 79 141 321 526 [...]
-Time to process a range of 3000 elements with std::[..] : 62.14389 us
-Time to process a range of 3000 elements with std::[..] : 69.27212 us
-$> ./PmergeMe "-1" "2"
-Error
-$> # For OSX USER:
-$> ./PmergeMe `jot -r 3000 1 100000 | tr '\n' ' '`
-[...]
-$>
-
+ Linux:
 ./PmergeMe `shuf -i 1-100000 -n 3000 | tr "\n" " "`
-
 */
 
 #include <iostream>
-#include <vector>
 #include <climits>
 #include <cstdlib>
 #include <ctime>
+#include <vector>
 #include <deque>
 
 #define SPLITSIZE 5
